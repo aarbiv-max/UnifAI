b340f7dff772a0eab0f4bcdae9076912b71b56f1
Eric Guanghui Zhou <eric.guanghui.zhou@nokia.com>
2025-05-02 20:27:20 -0400

    change the ci groovy logic to use credential

--- ci/genie-deployment-RAG-fix.groovy
+++ ci/genie-deployment-RAG-fix.groovy
@@ -403,7 +403,10 @@ pipeline {
                     script {
                       module = "helmfile"
                       cleanWorkspace(module) 
-                      withCredentials([string(credentialsId: 'RHOI-service-token', variable: 'token')]){
+                      withCredentials([
+                        string(credentialsId: 'RHOI-service-token', variable: 'token'),
+                        string(credentialsId: 'HF_TOKEN', variable: 'HF_TOKEN')
+                      ]){
 
                         echo("Creating helm deployment pod")
                         sh("oc login --token=${token} --server=${ClusterAddress}")
