1a9a907f6e3cd506a28f153761ea333c41eee574
sfiresht <sfiresht@redhat.com>
2025-06-22 19:22:45 +0300

    working on the "be"

--- multi-agent/entrypoint.sh
+++ multi-agent/entrypoint.sh
@@ -10,21 +10,24 @@ echo "------------------------------------------"
 case "$ROLE" in
   flask)
     echo "🟢 Starting Flask API (Server)..."
-    . ~/backend/venv/bin/activate
-    exec gunicorn -w 4 -b 0.0.0.0:port run.wsgi:application
+    . ~/multi-agent/venv/bin/activate
+    exec gunicorn -w 4 -b 0.0.0.0:$APP_PORT run.wsgi:application
     ;;
     
   celery)
     echo "🔧 Starting Slack Celery worker with tasks concurrently : $CELERY_WORKER"
-    . ~/backend/venv/bin/activate
-    #exec celery -A celery_app.init worker -c $CELERY_WORKER --loglevel=info -Q $CELERY_QUEUES -n data_sources
-    echo "this is a place holder for the celery"
-    exit 0
+    . ~/multi-agent/venv/bin/activate
+    exec celery -A celery_app.init worker -c $CELERY_WORKER --pool=solo --loglevel=info -Q $CELERY_QUEUES -n data_sources
+    ;;
+
+  debug)
+    echo "🐞 Debug mode activated — container will stay alive."
+    tail -f /dev/null
     ;;
 
   *)
     echo "❌ ERROR: Unknown ROLE \"$ROLE\""
-    echo "Valid roles are: flask, celery"
+    echo "Valid roles are: flask, celery, debug"
     exit 1
     ;;
 esac
