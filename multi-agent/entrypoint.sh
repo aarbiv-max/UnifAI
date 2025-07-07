a1ba61ea7f4abe119b86c07895f08d994dec0f7e
sfiresht <sfiresht@redhat.com>
2025-07-07 13:28:19 +0300

    multiple changes for the MR

--- multi-agent/entrypoint.sh
+++ multi-agent/entrypoint.sh
@@ -10,13 +10,13 @@ echo "------------------------------------------"
 case "$ROLE" in
   flask)
     echo "🟢 Starting Flask API (Server)..."
-    . ~/multi-agent/venv/bin/activate
-    exec gunicorn -w 4 -b 0.0.0.0:$APP_PORT run.wsgi:application
+    . ~/venv/bin/activate
+    exec gunicorn -w 4 -b 0.0.0.0:$APP_PORT --chdir ~/app run.wsgi:application
     ;;
     
   celery)
     echo "🔧 Starting Slack Celery worker with tasks concurrently : $CELERY_WORKER"
-    . ~/multi-agent/venv/bin/activate
+    . ~/venv/bin/activate
     exec celery -A celery_app.init worker -c $CELERY_WORKER --pool=solo --loglevel=info -Q $CELERY_QUEUES -n multiagent
     ;;
 
