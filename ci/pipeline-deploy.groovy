11d6a689bb1fecf95616ebc0ebd91885a0f7f55b
Yosi Habushi <yhabushi@redhat.com>
2025-09-03 17:04:35 +0300

    add sso module to list when deployment removed.

--- ci/pipeline-deploy.groovy
+++ ci/pipeline-deploy.groovy
@@ -121,7 +121,7 @@ def deployModules(module){
 def deleteRunningApplication(){
     echo("Removing running UnifAI application")
 
-    def charts = ["dataflow", "multiagent", "shared-resources","ui"]
+    def charts = ["dataflow", "multiagent", "shared-resources","ui", "sso"]
 
     charts.each { chart ->
         sh("podman exec -t helmfile bash -c 'helmfile destroy -f ${chart}.yaml.gotmpl --deleteWait'")
