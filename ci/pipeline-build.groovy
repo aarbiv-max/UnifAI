41aa0ae55fd84a963803fe3cc4777342ccb3a89d
yhabushi <yhabushi@redhat.com>
2025-09-10 22:22:04 +0300

    another try

--- ci/pipeline-build.groovy
+++ ci/pipeline-build.groovy
@@ -46,8 +46,9 @@ def buildDockerImage(String component) {
         context = "${component}"
         // Replace the placeholder __APP_VERSION__ with the VERSION from Jenkins
         sh """
-            sed -i "s|__APP_VERSION__|\"${VERSION}\"|g" ${context}/client/src/components/shared/HelpPanel.tsx
+            sed -i "s|__APP_VERSION__|\\\"${VERSION}\\\"|g" ${context}/client/src/components/shared/HelpPanel.tsx
         """
+
     }
 
     String logFile = "/tmp/${component.replace("/", "_")}_build.log"
