d6e65cca69cd47aeefb1733975956ba765368c4e
Yosi Habushi <yhabushi@redhat.com>
2025-09-10 15:38:35 +0300

    revert changes in ui

--- DataPipelineHub/ui/deployment/entrypoint.sh
+++ DataPipelineHub/ui/deployment/entrypoint.sh
@@ -1,7 +1,4 @@
 #!/bin/sh
 export DOLLAR='$'
-if [ -n "$MODULE_VERSION" ]; then
-  echo "window.__MODULE_VERSION__='$MODULE_VERSION';" > /usr/share/nginx/html/version.js
-fi
 envsubst < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
 exec nginx -g 'daemon off;'
