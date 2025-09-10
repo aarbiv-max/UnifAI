9909c89cd8c304df7878c61aa436400eb6422925
Yosi Habushi <yhabushi@redhat.com>
2025-09-10 15:56:29 +0300

    last attempt

--- DataPipelineHub/ui/deployment/entrypoint.sh
+++ DataPipelineHub/ui/deployment/entrypoint.sh
@@ -1,4 +1,11 @@
 #!/bin/sh
 export DOLLAR='$'
 envsubst < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
+if [ -f /usr/share/nginx/html/version.js.template ]; then
+    envsubst < /usr/share/nginx/html/version.js.template > /usr/share/nginx/html/version.js
+    echo "Generated version.js with MODULE_VERSION=$MODULE_VERSION"
+else
+    echo "version.js.template not found, skipping..."
+fi
+
 exec nginx -g 'daemon off;'
