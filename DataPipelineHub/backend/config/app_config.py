1285c85253e2b533b126c581d1e4fbed27dcabda
yhabushi <yhabushi@redhat.com>
2025-11-13 13:31:40 +0200

    firt attempt to use docling remote service

--- DataPipelineHub/backend/config/app_config.py
+++ DataPipelineHub/backend/config/app_config.py
@@ -15,6 +15,8 @@ class AppConfig(SharedConfig):
 
     qdrant_ip: str = "0.0.0.0"
     qdrant_port: str = "6333"
+    
+    docling_service_url: str = "0.0.0.0"
 
     # Slack Configuration
     default_slack_bot_token: str = "xoxb-2253118358-8783454711008-dwnxf7cPBpeVLlLw8KMurohb"
